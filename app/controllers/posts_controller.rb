e0238174e2173ba731585faa43503438fa2a1ab4
Anil Reddy <anil.reddy@bacancytechnology.com>
2019-09-12 19:53:35 +0530

    complete_posts_task

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,27 @@
 class PostsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
+  # before_action :owned_post, only: [:edit, :update, :destroy]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
   end
+  
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    if @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
       flash[:success] = "Your post has been created!"
       redirect_to posts_path
     else
-      flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+      flash[:alert] = "Your new post couldn't be created!  Please check the form."
       render :new
     end
-  end
+  end  
 
   def show
     @post = Post.find(params[:id])
@@ -43,13 +47,20 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+
   private
 
   def post_params
-    params.require(:post).permit(:image, :caption)
+    params.require(:post).permit(:image, :caption, :user_id)
   end
 
   def set_post
     @post = Post.find(params[:id])
   end
+  # def owned_post
+  #   unless current_user == @post.user
+  #     flash[:alert] = "That post doesn't belong to you!"
+  #     redirect_to root_path
+  #   end
+  # end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ApplicationRecord
+  validates :user_id, presence: true
   validates :image, presence: true
 
   # has_attached_file :image, styles: { :medium => "640x" }
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  belongs_to :user
 end
