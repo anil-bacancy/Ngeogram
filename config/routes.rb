6942f97fa46205c8398992ce2175f7d4ec96fe44
anil reddy <anilroyal867@gmail.com>
2019-09-16 10:16:01 +0530

    last_follow_attempt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'profiles/show'
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users , controllers: { registrations: 'registrations' }
   resources :posts do
     resources :comments
   end
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
   patch ':user_name/edit', to: 'profiles#update', as: :update_profile
   root to: 'posts#index'
+
+  resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
